2b811f8ee377dbefc0385b5c899ceaee9fd0da2d
kyungstar <azure0563@gmail.com>
2023-05-11 00:16:56 +0900

    Java Section 2 Finish

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,34 +1,80 @@
 package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.user.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final List<User> users = new ArrayList<>();
+
+    private final JdbcTemplate jdbcTemplate; // 데이터베이스와 상호작용하기 위한 JdbcTemplate 객체
+
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate; // JdbcTemplate 객체를 클래스 변수에 할당
+    }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        // jdbcTemplate.update()는 INSERT, UPDATE, DELETE 쿼리에 사용할 수 있다.
+        // 첫 파라미터로는 sql을 받고, ?를 대신할 값을 차례로 넣으면 된다.
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        List<UserResponse> responses = new ArrayList<>();
-        for (int i = 0; i < users.size(); i++){
-            responses.add(new UserResponse(i + 1, users.get(i)));
-        }
-        return responses;
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+            @Override
+            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                return new UserResponse(id, name, age);
+            }
+        });
     }
 
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        String readSql = "SELECT * FROM user WHERE id = ?";
+
+        // (rs, rowNum) -> 0 SELECT SQL의 결과가 있으면 0으로 변환된다.
+        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
+
+        if(isUserNotExist){
+            throw new IllegalArgumentException();
+        }
+
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name) {
+        String readSql = "SELECT * FROM user WHERE name = ?";
+
+        // (rs, rowNum) -> 0 SELECT SQL의 결과가 있으면 0으로 변환된다.
+        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+
+        System.out.println(isUserNotExist);
+
+        if(isUserNotExist){
+            throw new IllegalArgumentException();
+        }
+
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
 
 }
