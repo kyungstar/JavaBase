d5ba539d88763ad82933b05630597a0c3e07ebb6
kyungstar <azure0563@gmail.com>
2024-05-07 22:43:21 +0900

    @Override 및 implements에 대한 설명 추가

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -4,8 +4,12 @@ import hello.hellospring.domain.Member;
 
 import java.util.*;
 
+
+
+// MemberRepository 인터페이스를 구현하는 MemoryMemberRepository 클래스
 public class MemoryMemberRepository implements MemberRepository {
 
+    // Map을 사용하여 데이터를 저장하는 저장소
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
@@ -15,6 +19,7 @@ public class MemoryMemberRepository implements MemberRepository {
      * 이는 컴파일러에게 해당 메소드가 실제로 상속된 메소드를 오버라이딩 하는 것이라고 알려줍니다.
      * 만약 해당 어노테이션을 사용하지 않고 메소드를 선언하면, 부모 클래스나 인터페이스에 정의된 메소드와 이름이 같은 새로운 메소드를 만들게 됩니다.
      */
+    // MemberRepository 인터페이스의 save 메서드를 구현
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
@@ -22,6 +27,7 @@ public class MemoryMemberRepository implements MemberRepository {
         return member;
     }
 
+    // MemberRepository 인터페이스의 findById 메서드를 구현
     @Override
     public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
@@ -32,6 +38,7 @@ public class MemoryMemberRepository implements MemberRepository {
      * HashMap 클래스는 키-값 쌍을 저장하는 해시맵을 구현한 클래스입니다.
      *  stream() 메소드 : HashMap의 키-값 쌍을 스트림으로 변환하여 다양한 작업을 수행할 수 있도록 합니다.
      */
+    // MemberRepository 인터페이스의 findByName 메서드를 구현
     @Override
     public Optional<Member> findByName(String name) {
         return store.values().stream()
@@ -39,12 +46,16 @@ public class MemoryMemberRepository implements MemberRepository {
                 .findAny();
     }
 
+    // MemberRepository 인터페이스의 findAll 메서드를 구현
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
 
 
+    // 저장소를 비우는 메서드
+    //  MemoryMemberRepository 클래스의 고유한 기능을 추가하는 데 사용됩니다.
+    //  인터페이스의 기본 목적을 따르는 것이 아니라, 클래스의 특수한 동작이나 상태 관리를 위한 추가적인 메서드입니다.
     public void clearStore() {
         store.clear();
     }
