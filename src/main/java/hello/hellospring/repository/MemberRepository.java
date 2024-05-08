9e6e5bc7758fbf62f4cdab62a5b9e83e6c7cae63
kyungstar <azure0563@gmail.com>
2024-05-08 23:34:31 +0900

    interface에 대한 설명 추가

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -5,6 +5,8 @@ import hello.hellospring.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
+
+// Java에서 인터페이스를 구현하는 클래스는 해당 인터페이스에 정의된 모든 메서드를 구현해야 합니다.
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
