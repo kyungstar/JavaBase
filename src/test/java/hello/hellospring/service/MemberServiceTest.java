a4f6bf9994e3c1cd83e9404b79a41fbbdebd13e9
kyungstar <azure0563@gmail.com>
2024-05-10 22:24:15 +0900

    오늘 했던거 설명좀 일부 추가해서 달아놓음

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -24,6 +24,10 @@ class MemberServiceTest {
     // `@BeforeEach` : 각 테스트 실행 전에 호출된다. 테스트가 서로 영향이 없도록 항상 새로운 객체를 생성하고, 의존관계도 새로 맺어준다.
     @BeforeEach
     public void beforeEach() {
+        // memberRepository를 새로운 MemoryMemberRepository 객체로 초기화하고, memberService를 이 새로운 memberRepository를 사용하여 초기화합니다.
+
+        // 이렇게 함으로써 각 테스트가 실행될 때마다 새로운 MemoryMemberRepository와 MemberService 객체가 생성되고, 이들 간의 의존 관계도 다시 맺어집니다.
+        // 이는 테스트의 독립성을 보장하여 테스트 결과가 예측 가능하고 일관되게 유지되도록 합니다.
         memberRepository = new MemoryMemberRepository();
         memberService = new MemberService(memberRepository);
     }
