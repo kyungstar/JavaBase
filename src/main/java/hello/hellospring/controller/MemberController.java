7dc554582ef91fe91764f7eb5b8b7c63c476cddb
kyungstar <azure0563@gmail.com>
2024-06-24 22:52:57 +0900

    13강 14강은 이해 잘 안됨, 다시 들어야함

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,12 +4,13 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-@Controller
 public class MemberController {
 
     private final MemberService memberService;
 
-    @Autowired
+
+    // 주의 : @Autowired를 통한, DI는 helloController, MemberService 등과 같이 스프링이 관리하는 객체에서만 동작한다.
+    // -> 스프링 빈으로 등록하지 않고 내가 직접 생성한 객체에서는 동작하지 않는다.
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
